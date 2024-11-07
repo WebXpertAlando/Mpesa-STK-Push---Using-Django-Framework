63027f342f795eed9ed2e06bfcaf915730a93d32
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2024-11-07 11:19:02 +0300

    Update views.py

--- MpesaApi/views.py
+++ MpesaApi/views.py
@@ -8,8 +8,8 @@ from .models import MpesaPayment
 
 
 def getAccessToken(request):
-    consumer_key = 'cHnkwYIgBbrxlgBoneczmIJFXVm0oHky'
-    consumer_secret = '2nHEyWSD4VjpNh2g'
+    consumer_key = ''
+    consumer_secret = ''
     api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 
     r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
